d2380e6020aa3059e70706db5c22edad12e4ea62
jms138-ua <jms138@gcloud.ua.es>
2020-09-24 20:12:12 +0200

    Implementacion JavaDocs y JUnit

--- prog3-battleship/src/model/Coordinate.java
+++ prog3-battleship/src/model/Coordinate.java
@@ -1,29 +1,37 @@
 package model;
 
+/** @author Javier Mellado Sanchez 48800386K */
+
 import java.util.Arrays;
 
+/**
+ * Clase que representa las coordenadas del juego,
+ * que son representadas en un array.
+ * Presenta los metodos basicos para operar
+ */
 
 public class Coordinate {
-
+	
+	/** Array que guarda las coordenadas (x,y) */
 	int[] components;
 	
-	
+	/** Constructor a partir de las coordenadas (x,y) */
 	public Coordinate(int x, int y) {
 		components = new int[2];
 		components[0] = x;
 		components[1] = y;
 	}
 	
-	
+	/** Constructor a partir de otro objeto Coordinate, copia sus coordenadas (x,y) */
 	public Coordinate(Coordinate c) {
 		components = new int[2];
-		
 		for (int i=0; i<components.length; i++) {
 			components[i] = c.components[i];
 		}
 	}
-	//_____________________________________________________________________________
+	//___________________________________________________________________________________
 	
+	/** Representar el objeto como un string de la forma "(x,y)" */
 	@Override
 	public String toString() {
 		StringBuilder concatenation = new StringBuilder();
@@ -40,7 +48,7 @@ public class Coordinate {
 		return concatenation.toString();
 	}
 	
-	
+	/** Identificador del objeto */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -49,7 +57,7 @@ public class Coordinate {
 		return result;
 	}
 
-
+	/** Comparar objeto o metodos de ambos objetos */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -64,7 +72,11 @@ public class Coordinate {
 		return true;
 	}
 
-
+	/**
+	 * Nueva coordenada a partir de la suma con otro objeto Coordinate
+	 * @param Objeto Coordinate para sumar
+	 * @return Un nuevo objeto Coordinate con sus coordenadas segun el resultado de la operacion
+	 */
 	public Coordinate add(Coordinate c) {
 		Coordinate new_c = new Coordinate(this);
 		
@@ -74,7 +86,11 @@ public class Coordinate {
 		return new_c;
 	}
 	
-	
+	/**
+	 * Nueva coordenada a partir de la resta con otro objeto Coordinate
+	 * @param Objeto Coordinate para restar
+	 * @return Un nuevo objeto Coordinate con sus coordenadas segun el resultado de la operacion
+	 */
 	public Coordinate subtract(Coordinate c) {
 		Coordinate new_c = new Coordinate(this);
 		
@@ -84,7 +100,7 @@ public class Coordinate {
 		return new_c;
 	}
 	
-	
+	/** Getter del array especificando coordenada (x o y) */
 	public int get(int component) {
 		if (component>=0 && component<components.length) {
 			return components[component];
@@ -95,7 +111,7 @@ public class Coordinate {
 		return -1;
 	}
 	
-	
+	/** Setter del array especificando coordenada (x o y) con su nuevo valor */
 	protected void set(int component, int value) {
 		if (component>=0 && component<components.length) {
 			components[component] = value;
